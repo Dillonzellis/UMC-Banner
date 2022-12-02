faaffc68e3e7af408c3d8acf510d872735b01de0
Dillon Ellis <dellis@fcul.com>
2022-12-02 11:55:48 -0500

    working carousel

---
+++ src/content/pages/carousel-testing.php
@@ -0,0 +1,278 @@
+<html>
+
+<head>
+    <title>United Methodist Connection Federal Credit Union - Home</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" />
+    <meta name="description"
+        content="Like all credit unions, United Methodist Connectional Federal Credit Union is a not-for-profit financial cooperative. When you join the credit union, you become a shareholder – or an owner – of a very unique financial institution.">
+    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+    <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+
+
+    <link rel="stylesheet" href="/css/carousel-testing.css">
+
+    <?php include 'inc/header-links.php'; ?>
+
+    <!-- <style>
+        .site-header {
+            position: relative;
+        }
+    </style> -->
+
+</head>
+
+<body>
+    <?php include 'inc/header.php'; ?>
+
+    <div id="wrap-all" class="wrap-all">
+        <div id="alert" style="color: white;
+    z-index: 99999999;
+    text-align: center;
+    font-weight: bold;
+    font-size: 18px;
+    background: black;
+	border: 1px solid #afcd5a;
+    border-bottom: 3px solid #afcd5a;">
+            <p style="    max-width: 990px;
+    margin: 0 auto;
+    padding: 20px 5px;"><img src="img/lscu-proud.png" alt="proud partner with lscu"></p>
+        </div>
+
+        <section class="">
+            <div class="container" style="max-width: 600px;padding: 0;">
+                <div class="row">
+                    <div class="col-lg-12" style="text-align: center;">
+                        <div style="padding:56.25% 0 0 0;position:relative;">
+                            <iframe src="https://player.vimeo.com/video/463115655?autoplay=1&muted=1"
+                                style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0"
+                                allow="autoplay; fullscreen" allowfullscreen>
+                            </iframe>
+                        </div>
+                        <script src="https://player.vimeo.com/api/player.js"></script>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- <div class="video-container">
+        <iframe src="https://player.vimeo.com/video/463115655?autoplay=1&muted=1" frameborder="0"
+            allow="autoplay; fullscreen" allowfullscreen>
+        </iframe>
+        <script src="https://player.vimeo.com/api/player.js"></script>
+    </div> -->
+
+        <section class="carousel-de">
+            <div class="container__wide">
+                <div class="slider-de">
+                    <div class="slide">
+                        <img src="/img/banner/new-carousel/carbuying-assistance 1.png" alt="" srcset="">
+                        <div class="slide__content-wrapper slide-light">
+                            <div class="slide__content">
+                                <div class="slide__content-header">Car Buying With Auto Assistance</div>
+                                <div class="slide__content-body">The new UMC FCU Auto Mall, powered by Auto Assistance
+                                </div>
+                                <div class="btn-wrapper">
+                                    <div class="slide__content-btn">Details</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="slide">
+                        <img src="/img/banner/new-carousel/carbuying-assistance 1.png" alt="" srcset="">
+                        <div class="slide__content-wrapper slide-light">
+                            <div class="slide__content">
+                                <div class="slide__content-header">Car Buying With Auto Assistance</div>
+                                <div class="slide__content-body">The new UMC FCU Auto Mall, powered by Auto Assistance
+                                </div>
+                                <div class="btn-wrapper">
+                                    <div class="slide__content-btn">Details</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="slide">
+                        <img src="/img/banner/new-carousel/carbuying-assistance 1.png" alt="" srcset="">
+                        <div class="slide__content-wrapper slide-light">
+                            <div class="slide__content">
+                                <div class="slide__content-header">Car Buying With Auto Assistance</div>
+                                <div class="slide__content-body">The new UMC FCU Auto Mall, powered by Auto Assistance
+                                </div>
+                                <div class="btn-wrapper">
+                                    <div class="slide__content-btn">Details</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="slide">
+                        <img src="/img/banner/new-carousel/carbuying-assistance 1.png" alt="" srcset="">
+                        <div class="slide__content-wrapper slide-light">
+                            <div class="slide__content">
+                                <div class="slide__content-header">Car Buying With Auto Assistance</div>
+                                <div class="slide__content-body">The new UMC FCU Auto Mall, powered by Auto Assistance
+                                </div>
+                                <div class="btn-wrapper">
+                                    <div class="slide__content-btn">Details</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <div class="container">
+                <div class="row">
+                    <!-- Icon box start //-->
+                    <div class="col-lg-3">
+                        <div class="service-3">
+                            <div class="inner">
+                                <i class="fa fa-flask"></i>
+                                <h2>apply for a loan</h2>
+                                <div class="cl"></div>
+                                <div class="line"></div>
+                                <div class="cl"></div>
+                                <p><a href="https://www.unitedmethodistcu.com/pages/loan-application.php"
+                                        class="biss-btn biss-btn-secondary" title="Apply Now">apply now</a></p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Icon box end //-->
+
+                    <!-- Icon box start //-->
+
+                    <div class="col-lg-3">
+                        <div class="service-3">
+                            <div class="inner">
+                                <i class="fa fa-briefcase"></i>
+                                <h2>online loan payments</h2>
+                                <div class="cl"></div>
+                                <div class="line"></div>
+                                <div class="cl"></div>
+                                <p><a href="https://unitedmethodistcu.alliedpayment.com/PortalPay/unitedmethodistcu/CustomerLoan.html"
+                                        class="biss-btn biss-btn-secondary" title="Pay Now">pay now</a></p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Icon box end //-->
+
+                    <!-- Icon box start //-->
+
+                    <div class="col-lg-3">
+                        <div class="service-3">
+                            <div class="inner">
+                                <i class="fa fa-lightbulb-o"></i>
+                                <h2>Shared branch locator</h2>
+                                <div class="cl"></div>
+                                <div class="line"></div>
+                                <div class="cl"></div>
+                                <p><a href="#" data-remodal-target="co-op" title="Find One"
+                                        class="biss-btn biss-btn-secondary">find one</a></p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Icon box end //-->
+
+                    <!-- Icon box start //-->
+
+                    <div class="col-lg-3">
+                        <div class="service-3">
+                            <div class="inner">
+                                <i class="fa fa-life-ring"></i>
+                                <h2>news &amp; events</h2>
+                                <div class="cl"></div>
+                                <div class="line"></div>
+                                <div class="cl"></div>
+                                <p><a href="https://www.unitedmethodistcu.com/pages/news.php"
+                                        class="biss-btn biss-btn-secondary" title="Learn More">learn more</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Icon box end //-->
+                </div>
+            </div>
+        </section>
+
+        <section class="secondary-bg-lighten padding-top-20 padding-bottom-40">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="action-block-2">
+                            <p class="main">Watch this week’s featured video, Acing the Job Interview</p>
+                            <p><a href="#" data-remodal-target="acing"><img src="img/Thumbnail9.png" width="200"
+                                        height="113" alt="" /></a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <?php include 'inc/footer.php'; ?>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.js"></script>
+        <script>
+            jQuery(document).ready(function () {
+                jQuery('.slider-de').slick({
+                    dots: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    autoplay: true,
+                    autoplaySpeed: 2000,
+                    arrows:false,
+                });
+            });
+        </script>
+
+
+        <?php include 'inc/modals.php'; ?>
+
+
+        <!-- JS includes //-->
+        <script src="https://www.unitedmethodistcu.com/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+        <!-- <script src="https://www.unitedmethodistcu.com/js/vendor/jquery-1.11.1.min.js"></script> -->
+        <script src="https://www.unitedmethodistcu.com/js/vendor/bootstrap.min.js"></script>
+        <script src="https://www.unitedmethodistcu.com/js/imagesloaded.pkgd.min.js"></script>
+        <script src="https://www.unitedmethodistcu.com/js/masonry.pkgd.min.js"></script>
+        <script src="https://www.unitedmethodistcu.com/js/less-1.7.4.min.js"></script>
+        <script src="https://www.unitedmethodistcu.com/js/jquery.colorbox-min.js"></script>
+        <script src="https://www.unitedmethodistcu.com/js/jquery.easing.1.3.js"></script>
+        <script src="https://www.unitedmethodistcu.com/js/jquery.rivathemes.js"></script>
+        <script src="https://www.unitedmethodistcu.com/js/layerslider/jquery-easing-1.3.js" type="text/javascript">
+        </script>
+        <script src="https://www.unitedmethodistcu.com/js/layerslider/jquery-transit-modified.js"
+            type="text/javascript">
+        </script>
+        <script src="https://www.unitedmethodistcu.com/js/layerslider/layerslider.transitions.js"
+            type="text/javascript">
+        </script>
+        <script src="https://www.unitedmethodistcu.com/js/layerslider/layerslider.kreaturamedia.jquery.js"
+            type="text/javascript"></script>
+        <script src="https://www.unitedmethodistcu.com/js/biss.js"></script>
+
+        <script type="text/javascript"
+            src="https://www.unitedmethodistcu.com/pages/Lively-Validator/livelyValidator.js">
+        </script>
+        <script type="text/javascript">
+            $(document).ready(function () {
+                $('#App').livelyValidator();
+            });
+        </script>
+
+
+        <script>
+            window.jQuery || document.write(
+                '<script src="https://www.unitedmethodistcu.com/remodal/libs/jquery/dist/jquery.min.js"><\/script>')
+        </script>
+        <script src="https://www.unitedmethodistcu.com/remodal/dist/remodal.js"></script>
+
+</body>
+
+</html>
